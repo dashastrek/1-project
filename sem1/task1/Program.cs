32ae0d3fd20aeca1471111111b7d5f8e68594f0d
Daria Strekopytova <frau.strekopytova2010@yandex.ru>
2022-10-17 21:42:40 +0300

    task3

---
+++ sem1/task1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число для поиска квадрата");
+string numberA = Console.ReadLine();
+int number = int.Parse(numberA);
+int result = number * number;
+
+Console.WriteLine($"Kvadrat {number} is {result}");
+
+
---
+++ sem1/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine ("Введите 2 числа");
+string numberA = Console.ReadLine();
+string numberB = Console.ReadLine();
+int numberAA = int.Parse(numberA);
+int numberBB = int.Parse(numberB);
+int result = numberBB * numberBB;
+if(result == numberAA)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
+
+
+
+ // На вход два числа, проверка второе это квадрат первого?
